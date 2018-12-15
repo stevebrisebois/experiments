afde0bfe73f3f2f608e7532c17b9299a1b7c0261
Steve Brisebois <sbrisebois@scolab.com>
2018-12-15 10:48:15 -0500

    Add 11 squares solution

--- stories/squaresSandbox.js
+++ stories/squaresSandbox.js
@@ -14,11 +14,24 @@ storiesOf('Squares Sandbox', module)
         rows="37"/>
     )
   ).add(
-  '37 x 72',
-  () => (
-    <BoardStandalone
-      size="12"
-      cols="37"
-      rows="72"/>
-  )
+    '37 x 72',
+    () => (
+      <BoardStandalone
+        size="12"
+        cols="37"
+        rows="72"/>
+    )
+  ).add(
+    '11 squares',
+    () => (
+      <img
+        width={72 * 12}
+        height={37 * 12}
+        src="https://screenshots.scolab.com/Public/Jing/SB/chrome_2018-12-14_16-08-06.png"
+      />
+    )
 )
+
+
+
+
